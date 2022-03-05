eae409752ceef9eb4c2c738b1efe840d16e82bc5
Jacqui <jt.cope1@gmail.com>
2022-03-05 13:47:51 +0800

    adds category as foreign key to listing table and model relationship

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,5 @@
 class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :brand
+  belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_05_035943) do
+ActiveRecord::Schema.define(version: 2022_03_05_050337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,9 @@ ActiveRecord::Schema.define(version: 2022_03_05_035943) do
     t.bigint "brand_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id", null: false
     t.index ["brand_id"], name: "index_listings_on_brand_id"
+    t.index ["category_id"], name: "index_listings_on_category_id"
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
@@ -59,5 +61,6 @@ ActiveRecord::Schema.define(version: 2022_03_05_035943) do
   end
 
   add_foreign_key "listings", "brands"
+  add_foreign_key "listings", "categories"
   add_foreign_key "listings", "users"
 end
