855b03558dd9e043aa724d1b9b66ed22c9947704
Jacqui <jt.cope1@gmail.com>
2022-03-06 15:17:39 +0800

    Adds listing test data. Alter listings display and adds comments

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,8 +1,10 @@
 class ListingsController < ApplicationController
   def index
+    #To show all listings
     @listings = Listing.all
   end
 
   def show
+    #To show individual listing item
   end
 end
